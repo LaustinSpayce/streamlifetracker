eec75d771d1bba83d73f2d8d00b4dac8a894f729
Stuart Myers <stuart.myers@gmail.com>
2021-11-19 18:02:10 +0800

    add socket

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const counterRouter = require('./routes/counter');
 
-var app = express();
+const app = express();
+const io = require('socket.io');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
